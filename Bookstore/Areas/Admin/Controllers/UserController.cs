e16bbbc265d11e038977b168ad88618a6ac79835
Krzysztof <cristopherb95@yahoo.com>
2020-03-10 16:18:43 +0100

    Added user management section

---
+++ Bookstore/Areas/Admin/Controllers/UserController.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Bookstore.DataAccess.Repository.IRepository;
+using Bookstore.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Bookstore.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class UserController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public UserController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        #region API CALLS
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var users = _db.ApplicationUsers.Include(x => x.Company).ToList();
+            var userRoles = _db.UserRoles.ToList();
+            var roles = _db.Roles.ToList();
+            foreach (var user in users)
+            {
+                var roleId = userRoles.FirstOrDefault(x => x.UserId == user.Id).RoleId;
+                user.Role = roles.FirstOrDefault(x => x.Id == roleId).Name;
+                if (user.Company == null)
+                {
+                    user.Company = new Company()
+                    {
+                        Name = ""
+                    };
+                }
+            }
+
+            return Json(new { data = users });
+        }
+
+        [HttpPost]
+        public IActionResult LockUnlock([FromBody] string id)
+        {
+            var user = _db.ApplicationUsers.FirstOrDefault(x => x.Id == id);
+            if (user == null)
+            {
+                return Json(new { success = false, message = "Error while locking/unlocking" });
+            }
+            if (user.LockoutEnd!=null && user.LockoutEnd > DateTime.Now)
+            {
+                // User is locked
+                user.LockoutEnd = DateTime.Now;
+            }
+            else
+            {
+                user.LockoutEnd = DateTime.Now.AddYears(1000);
+            }
+            _db.SaveChanges();
+
+            return Json(new { success = true, message = "Operation successful" });
+        }
+
+        #endregion
+    }
+}
---
+++ Bookstore/wwwroot/js/user.js
@@ -0,0 +1,93 @@
+﻿var dataTable;
+
+$(document).ready(function () {
+    loadDataTable();
+});
+
+function loadDataTable() {
+    dataTable = $('#tblData').DataTable({
+        "ajax": {
+            "url": "/Admin/User/GetAll"
+        },
+        "columns": [
+            { "data": "name", "width": "15%" },
+            { "data": "email", "width": "15%" },
+            { "data": "phoneNumber", "width": "15%" },
+            { "data": "company.name", "width": "15%" },
+            { "data": "role", "width": "15%" },
+            {
+                "data": {
+                    id: "id",
+                    lockoutEnd: "lockoutEnd"
+                },
+                "render": function (data) {
+                    const today = new Date().getTime();
+                    const lockout = new Date(data.lockoutEnd).getTime();
+                    if (lockout > today) {
+                        // User is locked
+                        return `
+                        <div class="text-center">
+                            <a onclick=LockUnlock('${data.id}') class="btn btn-danger text-white" style="cursor: pointer;">
+                                <i class="fas fa-lock-open"></i> Unlock
+                            </a>
+                        </div>
+                    `;
+                    }
+                    else {
+                        return `
+                        <div class="text-center">
+                            <a onclick=LockUnlock('${data.id}') class="btn btn-success text-white" style="cursor: pointer;">
+                                <i class="fas fa-lock-open"></i> Lock
+                            </a>
+                        </div>
+                    `;
+                    }
+                }, "width": "25%"
+            }
+        ]
+    });
+}
+
+function LockUnlock(id) {
+    $.ajax({
+        type: "POST",
+        url: "/Admin/User/LockUnlock",
+        data: JSON.stringify(id),
+        contentType: "application/json",
+        success: function (data) {
+            if (data.success) {
+                toastr.success(data.message);
+                dataTable.ajax.reload();
+            }
+            else {
+                toastr.error(data.message);
+            }
+        }
+    });
+}
+
+function Delete(url) {
+    swal({
+        title: "Are you sure you want to delete?",
+        text: "You will not be able to restore the data",
+        icon: "warning",
+        buttons: true,
+        dangerMode: true
+    }).then((willDelete) => {
+        if (willDelete) {
+            $.ajax({
+                type: "DELETE",
+                url: url,
+                success: function (data) {
+                    if (data.success) {
+                        toastr.success(data.message);
+                        dataTable.ajax.reload();
+                    }
+                    else {
+                        toastr.error(data.message);
+                    }
+                }
+            });
+        }
+    });
+}
